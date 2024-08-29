4ac5a7753685c7e0141f2030b2ed84c6d681a6dd
Mukund <mukundmadhav054@gmail.com>
2024-08-29 16:29:30 +0530

    Array of Objects - Done

--- Main.java
+++ Main.java
@@ -1,22 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        //Creating objects for Animal Class
-        Animal lion = new Animal("Leo", 5, "Lion");
-        Animal zebra = new Animal("Zara", 3, "Zebra");
+        //Creating an array of Animal objects
+        Animal[] animals = new Animal[5];   //Array to store 5 Animal objects
+
+        //Instantiating Animal objects and storing them in an array
+        animals[0] = new Animal("Leo", 5, "Lion");
+        animals[1] = new Animal("Zara", 3, "Zebra");
+        animals[2] = new Animal("Ella", 2, "Elephant");
+        animals[3] = new Animal("Asteroid Destroyer", 1, "Cat");
+        animals[4] = new Animal("Bob", 2, "Rhinoceros");
 
         //Creating an object for Environment Class
         Environment savaana = new Environment("Summer", "Sunny");
 
-        //Using Animal objects to display details and simulate movement
-        lion.displayDetails();
-        lion.move();
-
-        System.out.println();   //Blank Line for readability
-
-        zebra.displayDetails();
-        zebra.move();
-
-        System.out.println();   //Blank Line for readability
+        // Looping through the array to display details and simulate movement
+        for (int i = 0; i < animals.length; i++) {
+            System.out.println("Animal " + (i + 1) + ":");
+            animals[i].displayDetails();
+            animals[i].move();
+            System.out.println();  // Blank line for better readability
+        }
 
         //Using Environment object to display details and simulate season change
         savaana.displayEnvironment();
